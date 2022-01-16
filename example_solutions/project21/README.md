880e4a82382ae86cd6de46d50a6f8f1b6a01a044
Constantin Pape <c.pape@gmx.net>
2022-01-16 11:58:08 +0100

    Finish project 21 example solution

---
+++ example_solutions/project21/README.md
@@ -0,0 +1,28 @@
+# Example Solution: Project 21
+
+This is an example solution for the myelin segmentation project.
+
+## Visualisation & Preparation
+
+- **data_preparation** Visualize the images and labels of the myelin dataset with [napari](https://napari.org/).
+- **data_preparation** Create a train / validation split. Note: once we have more images this needs to be updated and we should also create a test split.
+
+Alternative ideas: maybe downsampling the data by a factor of 2 might help for the downstream task to improve the effective field of view.
+
+## Training
+
+- **train_semantic_dice** Train a network for semantic segmentation of axon, myelin and tongue labels with the dice loss.
+- **train_boundaries** Train a network to predict foreground and boundary probabilities for the axons.
+
+The training is implemented with [torch_em](https://github.com/constantinpape/torch-em).
+Alternative ideas: train the semantic network with cross entropy or try a single network per semantic class.
+
+## Prediction & Postprocessing
+
+- **predict_and_segment** Run prediction on the test images and postprocess the results to obtain an instance and semantic segmentation.
+
+The instance segmentation is done via multicut segmentation using the [elf](https://github.com/constantinpape/elf) segmentation library.
+For semantic segmentation  the max class is assigned inside of segmented obejcts.
+
+Alternative ideas: maybe the boundary predictions are not required and the instance segmentation could be done from the predictions of the semantic network as well, by using the myelin channel as boundary probabilities and the combined axon, myelin and foreground probabilities as foreground channel. But this approach would be inferior if axons touch without discerinible myelin in between.
+Adanced: use a joint instance and semantic segmentation approach.
--- example_solutions/project21/predict_and_segment.py
+++ example_solutions/project21/predict_and_segment.py
@@ -4,9 +4,13 @@ from glob import glob
 
 import imageio
 import h5py
+import elf.segmentation as eseg
+import numpy as np
 from torch_em.util import get_trainer
 from torch_em.transform.raw import standardize
 from torch_em.util.prediction import predict_with_halo
+from skimage.measure import regionprops
+from skimage.segmentation import relabel_sequential
 
 # this export is needed to load the model again
 from train_semantic_dice import myelin_label_transform
@@ -51,20 +55,96 @@ def predict_boundaries(input_folder, output_folder, model_path):
         predict(input_path, output_path, output_keys, model, device)
 
 
-# TODO
-def segment(path):
-    pass
-    # with h5py.File(path, "a") as f:
-    #     pass
+def sharpen_predictions(predictions, percentile=95, clip=True):
+    """ Sharpen the predictions by dividing with a high percentile.
+    """
+    predictions /= np.percentile(predictions, percentile)
+    if clip:
+        predictions = np.clip(predictions, 0.0, 1.0)
+    return predictions
+
+
+def instance_segmentation(path):
+    with h5py.File(path, "r") as f:
+        foreground = f["predictions/foreground"][:]
+        boundaries = f["predictions/boundaries"][:]
+    # percentile based prediction normalization
+    foreground = sharpen_predictions(foreground)
+    boundaries = sharpen_predictions(boundaries)
+
+    # make superpixels via watershed
+    ws, max_id = eseg.distance_transform_watershed(boundaries, threshold=0.25, sigma_seeds=2.0)
+
+    # compute graph represenation and edge features
+    rag = eseg.compute_rag(ws, max_id+1, n_threads=4)
+    costs = eseg.compute_boundary_mean_and_length(rag, boundaries, n_threads=4)[:, 0]
+    costs = eseg.compute_edge_costs(costs)
+
+    # run multicut segmentation
+    node_labels = eseg.multicut.multicut_decomposition(rag, costs, n_threads=4)
+    if 0 in node_labels:  # avoid having a segment with id 0
+        node_labels += 1
+    segmentation = eseg.project_node_labels_to_pixels(rag, node_labels, n_threads=4)
+
+    # postprocessing to filter out background
+    fg_threshold = 0.5
+    props = regionprops(segmentation, foreground)
+    bg_ids = [prop.label for prop in props if prop.mean_intensity < fg_threshold]
+    postprocessed = segmentation.copy()
+    postprocessed[np.isin(segmentation, bg_ids)] = 0
+    postprocessed = relabel_sequential(postprocessed)[0]
+
+    with h5py.File(path, "a") as f:
+        ds = f.require_dataset(
+            "segmentation/multicut", shape=segmentation.shape, compression="gzip", dtype=segmentation.dtype
+        )
+        ds[:] = segmentation
+        ds = f.require_dataset(
+            "segmentation/postprocessed", shape=postprocessed.shape, compression="gzip", dtype=postprocessed.dtype
+        )
+        ds[:] = postprocessed
 
 
 def segment_instances(folder):
     files = glob(os.path.join(folder, "*.h5"))
     print("Run instance segmentation")
     for path in files:
-        segment(path)
+        instance_segmentation(path)
 
 
+def semantic_segmentation(path):
+    # load the isntance segmentation and the semantic predictions
+    with h5py.File(path, "r") as f:
+        instances = f["segmentation/postprocessed"][:]
+        pred_keys = ["axon", "tongue", "myelin"]
+        semantic_pred = []
+        for key in pred_keys:
+            semantic_pred.append(f[f"predictions/{key}"][:][None])
+        semantic_pred = np.concatenate(semantic_pred, axis=0)
+
+    # assing the max semantic label inside the instance mask
+    semantic_seg = np.argmax(semantic_pred, axis=0) + 1
+    # use the instance segmentation as foreground mask
+    semantic_seg[instances == 0] = 0
+
+    with h5py.File(path, "a") as f:
+        ds = f.require_dataset(
+            "segmentation/semantic", shape=semantic_seg.shape, dtype=semantic_seg.dtype, compression="gzip"
+        )
+        ds[:] = semantic_seg
+
+
+def segment_semantic(folder):
+    files = glob(os.path.join(folder, "*.h5"))
+    print("Run semantic segmentation")
+    for path in files:
+        semantic_segmentation(path)
+
+
+# alternative post-processing approaches:
+# maybe we don't need the semantic predictions here and can just use the myelin prediction as boundary channel
+# and use the combined axon, myelin and tongue predictions as foreground channel
+# this would however fail if there are two axons touching without myelin
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_folder")
@@ -76,6 +156,7 @@ def main():
     predict_semantic(args.input_folder, args.output_folder, args.semantic_model)
     predict_boundaries(args.input_folder, args.output_folder, args.boundary_model)
     segment_instances(args.output_folder)
+    segment_semantic(args.output_folder)
 
 
 if __name__ == "__main__":
