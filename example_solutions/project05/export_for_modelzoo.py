cec37be2a7f1cfab9d069ce88bbb6357dab99260
Constantin Pape <constantin.pape@embl.de>
2022-01-16 14:07:59 +0100

    Implement modelzoo export

--- example_solutions/project05/export_for_modelzoo.py
+++ example_solutions/project05/export_for_modelzoo.py
@@ -1,1 +1,33 @@
-# TODO
+import h5py
+from torch_em.util import export_bioimageio_model, get_default_citations, export_parser_helper
+
+# this export is needed to load the model again
+from train_semantic_dice import microct_label_transform
+
+
+def export_for_modelzoo(checkpoint, input_, output):
+    with h5py.File(input_, "r") as f:
+        input_data = f["image"][:]
+    name = "MicroCTSemanticSegmentation"
+    tags = ["unet", "micro-ct", "semantic-segmentation"]
+
+    cite = get_default_citations(model="UNet3d")
+    doc = "#MicroCT Segmentation Model"
+
+    export_bioimageio_model(
+        checkpoint, output, input_data,
+        name=name,
+        authors=[{"name": "Your Name"}],
+        tags=tags,
+        license="CC-BY-4.0",
+        documentation=doc,
+        git_repo="https://github.com/kreshuklab/teaching-dl-course-2022",
+        cite=cite,
+        input_optional_parameters=False,
+    )
+
+
+if __name__ == "__main__":
+    parser = export_parser_helper()
+    args = parser.parse_args()
+    export_for_modelzoo(args.checkpoint, args.input, args.output)
--- example_solutions/project21/export_for_modelzoo.py
+++ example_solutions/project21/export_for_modelzoo.py
@@ -1,1 +1,32 @@
-# TODO
+import imageio
+from torch_em.util import export_bioimageio_model, get_default_citations, export_parser_helper
+
+# this export is needed to load the model again
+from train_semantic_dice import myelin_label_transform
+
+
+def export_for_modelzoo(checkpoint, input_, output):
+    input_data = imageio.imread(input_)[:512, :512]
+    name = "AxonSemanticSegmentation"
+    tags = ["unet", "neurons", "semantic-segmentation", "electron-microscopy"]
+
+    cite = get_default_citations(model="UNet2d")
+    doc = "#Axon Segmentation Model"
+
+    export_bioimageio_model(
+        checkpoint, output, input_data,
+        name=name,
+        authors=[{"name": "Your Name"}],
+        tags=tags,
+        license="CC-BY-4.0",
+        documentation=doc,
+        git_repo="https://github.com/kreshuklab/teaching-dl-course-2022",
+        cite=cite,
+        input_optional_parameters=False,
+    )
+
+
+if __name__ == "__main__":
+    parser = export_parser_helper()
+    args = parser.parse_args()
+    export_for_modelzoo(args.checkpoint, args.input, args.output)
