d245ee17f553913319ae4c1fe3f97e93a5b96f36
Constantin Pape <c.pape@gmx.net>
2022-01-17 21:40:47 +0100

    Add solution snippets for project21

---
+++ example_solutions/project21/snippets/README.md
@@ -0,0 +1,5 @@
+# Solution snippets
+
+Sharing some solution snippets during the course:
+- label_preparation.py: merge the label "4" into the backgorund (label "0")
+- simple_unet.ipynb: unet exercise from webinar3 adapted for the myelin data
---
+++ example_solutions/project21/snippets/label_preparation.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+import imageio
+
+
+# this function loads the labels from tif,
+# merges the label "4" (= ignore label) into the background ("0")
+# and saves the label again
+def process_labels(label_path):
+    # load the labels into a numpy array
+    labels = imageio.imread(label_path)
+    # merge the label "4" into the background
+    labels[labels == 4] = 0
+    # write the new labes (this overwrites the image at label path)
+    imageio.imwrite(label_path, labels)
+
+
+# process all the label files
+def process_all_labels(input_folder):
+    # this will select all tif files in the folder "<input_folder>/train/labels"
+    train_labels = glob(os.path.join(input_folder, "train", "labels", "*.tif"))
+    for label_path in train_labels:
+        process_labels(label_path)
+    # this will select all tif files in the folder "<input_folder>/val/labels"
+    val_labels = glob(os.path.join(input_folder, "val", "labels", "*.tif"))
+    for label_path in val_labels:
+        process_labels(label_path)
+
+
+# adapt this path to where you have saved the data
+input_folder = ""
+process_labels(input_folder)
